117c91c0240885d6a5abc9bf62df1a567030329d
chetra_seng <sengchetra.love@icloud.com>
2021-09-08 19:33:33 +0700

    Added Primary Bean and Default Profile example

--- src/main/java/guru/springframework/sfgdi/config/GreetingServiceConfig.java
+++ src/main/java/guru/springframework/sfgdi/config/GreetingServiceConfig.java
@@ -1,9 +1,10 @@
 package guru.springframework.sfgdi.config;
 
-import guru.springframework.sfgdi.services.ConstructorGreetingService;
-import guru.springframework.sfgdi.services.PropertyInjectedGreetingService;
+import guru.springframework.sfgdi.services.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
 public class GreetingServiceConfig {
@@ -15,5 +16,23 @@ public class GreetingServiceConfig {
     @Bean
     PropertyInjectedGreetingService propertyInjectedGreetingService(){
         return new PropertyInjectedGreetingService();
+    }
+
+    @Primary
+    @Bean
+    PrimaryGreetingService primaryGreetingService(){
+        return new PrimaryGreetingService();
+    }
+
+    @Profile({"ES", "default"})
+    @Bean
+    I18NSpanishService i18nService(){
+        return new I18NSpanishService();
+    }
+
+    @Profile("EN")
+    @Bean("i18nService")
+    I18nEnglishGreetingService englishGreetingService(){
+        return new I18nEnglishGreetingService();
     }
 }
